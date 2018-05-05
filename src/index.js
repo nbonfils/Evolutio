4dc9618eee40324dd7f85f472f5527e1db383e75
nbonfils <nils.bonfils@epfl.ch>
2018-05-05 16:51:37 +0200

    Finish start screen

--- src/index.js
+++ src/index.js
@@ -4,19 +4,24 @@ import * as PIXI from 'pixi.js';
 import StartScreen from './assets/startScreen.png';
 
 const app = new PIXI.Application({
-  width: 1078,
-  height: 728,
+  width: 960,
+  height: 540,
 });
 document.body.appendChild(app.view);
+
+// glowing effect for a text
+const glowingEffect = (text, count) => () => {
+  count += 0.03;
+
+  let alpha = Math.sin(count);
+
+  text.alpha = (alpha + 1) / 2;
+};
 
 /**
  * Initialize the game
  */
 function init() {
-  // add functions to the ticker
-  app.ticker.add((delta) => gameLoop(delta));
-
-
   // load the starting screen background
   const background = PIXI.Sprite.fromImage(StartScreen);
 
@@ -26,45 +31,77 @@ function init() {
   app.stage.addChild(background);
 
 
-  // the title of the game
+  // the title text
   const startScreenTitle = new PIXI.Container();
-  const text = new PIXI.Text('eVOLUTIO', {
+  const startScreenText = new PIXI.Text('eVOLUTIO', {
     fontFamily: 'acherus_grotesqueregular',
     fontSize: 50,
     fill: 'white',
   });
-  const blured = new PIXI.Text('eVOLUTIO', {
+  const startScreenBlured = new PIXI.Text('eVOLUTIO', {
     fontFamily: 'acherus_grotesqueregular',
     fontSize: 50,
     fill: 0xd7f4e3,
   });
+
+  app.ticker.add(glowingEffect(startScreenBlured, 0));
+
+  // apply blur to one of the text occurence
   const blurFilter = new PIXI.filters.BlurFilter();
+  startScreenBlured.filters = [blurFilter];
 
-  // make the blur "glow"
-  let count = 0;
-  app.ticker.add(() => {
-    count += 0.03;
+  startScreenTitle.addChild(startScreenText);
+  startScreenTitle.addChild(startScreenBlured);
 
-    let alpha = Math.sin(count);
-
-    blured.alpha = (alpha + 1) / 2;
-  });
-
-  blured.filters = [blurFilter];
-
-  startScreenTitle.addChild(text);
-  startScreenTitle.addChild(blured);
-
+  // center the title
   startScreenTitle.pivot.set(
     startScreenTitle.width / 2,
     startScreenTitle.height / 2
   );
   startScreenTitle.position.set(
     app.renderer.width / 2,
-    app.renderer.height / 4
+    app.renderer.height / 6
   );
 
   app.stage.addChild(startScreenTitle);
+
+
+  // "Press a key to start" text
+  const keyToStartContainer = new PIXI.Container();
+  const keyToStartText = new PIXI.Text('Press a key to start', {
+    fontFamily: 'acherus_grotesqueregular',
+    fontSize: 40,
+    fill: 'white',
+  });
+  const keyToStartBlurred = new PIXI.Text('Press a key to start', {
+    fontFamily: 'acherus_grotesqueregular',
+    fontSize: 40,
+    fill: 0xd7f4e3,
+  });
+
+  app.ticker.add(glowingEffect(keyToStartBlurred, 0));
+
+  // apply blur to one of the text occurence
+  keyToStartBlurred.filters = [blurFilter];
+
+  keyToStartContainer.addChild(keyToStartText);
+  keyToStartContainer.addChild(keyToStartBlurred);
+
+  // center the text
+  keyToStartContainer.pivot.set(
+    keyToStartContainer.width / 2,
+    keyToStartContainer.height / 2
+  );
+  keyToStartContainer.position.set(
+    app.renderer.width / 2,
+    app.renderer.height - (app.renderer.height / 6)
+  );
+
+  app.stage.addChild(keyToStartContainer);
+
+
+  // main game loop
+  app.ticker.add((delta) => gameLoop(delta));
 }
 
 /**

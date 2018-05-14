6859453aaaccd80deeb052322379484f1510ccbe
nbonfils <nils.bonfils@epfl.ch>
2018-05-14 17:52:47 +0200

    Not working movement physics for the cell

--- src/Cell.js
+++ src/Cell.js
@@ -22,7 +22,14 @@ export default class Cell extends PIXI.Container {
     this.ticker = new PIXI.ticker.Ticker();
     this.speedX = 0;
     this.speedY = 0;
+    this.accX = 0;
+    this.accY = 0;
+
+    // Bind the functions
+    this.destroy = this.destroy.bind(this);
     this.move = this.move.bind(this);
+    this.leftAcc = this.leftAcc.bind(this);
+    this.leftDec = this.leftDec.bind(this);
 
     this.cellSprite.anchor.set(0.5, 0.5);
     this.coreSprite.anchor.set(0.5, 0.5);
@@ -50,7 +57,33 @@ export default class Cell extends PIXI.Container {
    * Update the position of the cell each tick
    */
   move() {
+    this.speedX += this.accX;
+    this.speedY += this.accY;
     this.x += this.speedX;
     this.y += this.speedY;
+  }
+
+  /**
+   * Left movement with smooth acceleration
+   */
+  leftAcc() {
+    console.log(this.speedX);
+    if (this.speedX <= -5) {
+      this.accX = 0;
+      this.speedX = 5;
+    } else {
+      this.accX = -0.25;
+    }
+  }
+
+  /**
+   * decelerate when left is released
+   */
+  leftDec() {
+    if (this.speed >= 0) {
+      this.accX = 0;
+    } else {
+      this.accX = 0.1;
+    }
   }
 }
